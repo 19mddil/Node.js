dafe108cd0bb41a20712c8122e43e0ca77522b55
19mddilAspire <mddilshadul98@gmail.com>
2022-09-09 03:58:48 +0600

    middle ware and req res cycle

--- myexpress/app.js
+++ myexpress/app.js
@@ -3,10 +3,24 @@ const app = express();
 const studentRouter = require('./routers/studentRouter');
 
 app.use(express.json());
-
+app.use((req, res, next) => {
+    console.log("I am middleware 1");
+    next();
+})
+app.use((req, res, next) => {
+    console.log("I am middleware 2");
+    next();
+});
 app.use('/api/students', studentRouter);
 
+app.get('/', (req, res, next) => {
+    // res.send("Another response!");
+    next();
+})
+
+
 app.get('/', (req, res) => {
+    console.log("I am get request middleware");
     res.send("Hello from express");
 });
 
