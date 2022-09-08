947d9c2691edfacb4bef35712637410bc1619617
19mddilAspire <mddilshadul98@gmail.com>
2022-09-09 04:08:40 +0600

    static file management

--- myexpress/app.js
+++ myexpress/app.js
@@ -3,6 +3,9 @@ const app = express();
 const studentRouter = require('./routers/studentRouter');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.use((req, res, next) => {
     console.log("I am middleware 1");
     next();
