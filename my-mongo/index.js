21041fef31e1f04f7450ceaf0d6a8a8ea79de0be
19mddilAspire <mddilshadul98@gmail.com>
2022-09-12 15:49:30 +0600

    mongo schema

---
+++ my-mongo/index.js
@@ -0,0 +1,25 @@
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/my-students', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to mongodb successfully");
+    })
+    .catch(() => {
+        console.log("Connection failed");
+    })
+
+const studentSchema = new mongoose.Schema({
+    firstName: { type: String },
+    lastName: String,
+    dob: Date,
+    entryDate: { type: Date, default: Date.now },
+    passed: Boolean,
+    hobbies: [String],// all entries will be string here,
+    parents: {
+        father: String,
+        mother: String,
+    },
+    subjects: [{ name: String, mark: { type: Number, min: 0, max: 100 } }],
+
+});
