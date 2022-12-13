3726e8ac2a0ba4c8dd629bfd67d462922c7c1432
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2022-12-13 21:32:20 +0600

    some notes

---
+++ Notes/basics.md
@@ -0,0 +1,10 @@
+# Global Object
+## Code
+```js
+    global.console.log("hello");
+```
+## Output
+```bash
+mddilshad@mddilshad-HP-ProBook-450-G5:~/Desktop/nodejs/Notes$ node index
+hello
+```
---
+++ Notes/circle.js
@@ -0,0 +1,11 @@
+const area = r => 3.1416 * r * r;
+const circumference = r => 2 * 3.1416 * r;
+const pi = 3.1416;
+
+console.log(module);
+
+module.exports.area = area;
+module.exports.circumference = circumference;
+module.exports.pi = pi;
+
+console.log(module);
