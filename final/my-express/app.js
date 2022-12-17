dabe8acf4a0368618f09a4bd72ff5a9404148e9e
19mddilAspire <mddilshadul98@gmail.com>
2022-12-17 19:39:17 +0600

    all about middlewares(built in,custom, thirdparty)

--- final/my-express/app.js
+++ final/my-express/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const studentRouter = require('./routers/studentRouter');
 
-app.use(express.json());
+app.use(express.json()); //POST/PATCH/PUT -> json body -> req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
 app.use('/api/students', studentRouter);
 
 app.get('/', (req, res) => {
