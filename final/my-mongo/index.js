1b5810e692bc4ad0573acfba6a3ba47cf8d27b5d
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2022-12-24 17:17:26 +0600

    custom validation

--- final/my-mongo/index.js
+++ final/my-mongo/index.js
@@ -1,5 +1,7 @@
-const db = require('./database');
 const student = require('./student');
 
-db.connectDB();
-student.saveStudent();
+student.createStudent();
+
+// student.updateStudent("63a6c2bd66197577410c8da1");
+// student.deleteStudent("63a6db7d6637ebde40d16957");
+// student.fetchStudents();
--- final/my-mongo/student.js
+++ final/my-mongo/student.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
+const db = require('./database');
+
+db.connectDB();
 
 const studentSchemaType = {
     firstName: String,
-    lastName: String,
+    lastName: { type: String, required: [true, "please enter last name"] },
     dob: Date,
     entryDate: { type: Date, default: Date.now },
     passed: Boolean,
-    hobbies: [String],
+    hobbies: {
+        type: Array,
+        of: String,
+        validate: {
+            validator: value => value.length > 0,
+            message: "There must be at least one hobby my man"
+        }
+    },
     parents: {
         father: String,
         mother: String,
@@ -19,35 +29,69 @@ const studentSchema = new mongoose.Schema(studentSchemaType);//The Schema is def
 
 const Student = mongoose.model('Students', studentSchema);//The Schema is converted to class
 
-const student = new Student({
-    firstName: "Rahim",
-    lastName: "Ahmed",
-    dob: new Date("27 April 1990"),
-    passed: true,
-    hobbies: ["Kiting", "Forest Bathing"],
-    parents: {
-        father: "Rahman",
-        mother: "Alia",
-    },
-    subjects: [
-        {
-            name: "Bangla",
-            marks: 90,
-        },
-        {
-            name: "Astrology",
-            marks: 100,
-        }
-    ]
-})
-
-const saveStudent = async () => {
+const createStudent = async () => {
     try {
-        let data = await student.save();
+        let data = await Student.create({
+            firstName: "Keanu",
+            dob: new Date("27 April 89"),
+            passed: true,
+            hobbies: [],
+            parents: {
+                father: "Rahman",
+                mother: "Alia",
+            },
+            subjects: [
+                {
+                    name: "Bangla",
+                    marks: 90,
+                },
+                {
+                    name: "Astrology",
+                    marks: 87,
+                }
+            ]
+        })
         console.log(data);
     } catch (e) {
-        return e.message;
+        for (field in e.errors) {
+            console.log(e.errors[field].message);
+        }
     }
 }
 
-module.exports.saveStudent = saveStudent;
+const fetchStudents = async () => {
+    try {
+        let studentData = await Student.find().select({ firstName: 1, lastName: 1, passed: 1 });
+        console.log(studentData);
+    } catch (e) {
+        console.log(e._message);
+    }
+}
+
+const updateStudent = async (id) => {
+    try {
+        fetchStudents();
+        let updatedStudent = await Student.updateOne({ _id: id }, {
+            $set: { passed: false }
+        });
+        fetchStudents();
+        console.log(updatedStudent);
+    } catch (e) {
+        console.log(e._message);
+    }
+}
+
+const deleteStudent = async (id) => {
+    try {
+        fetchStudents();
+        await Student.deleteOne({ _id: id });
+        fetchStudents();
+    } catch (e) {
+        console.log(e._message);
+    }
+}
+
+module.exports.createStudent = createStudent;
+module.exports.fetchStudents = fetchStudents;
+module.exports.updateStudent = updateStudent;
+module.exports.deleteStudent = deleteStudent;
