bd031b09965158a32f738d2fb0e2811d02d47166
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2023-01-01 02:42:43 +0600

    initialization

--- sql/node-sql-test/index.js
+++ sql/node-sql-test/index.js
@@ -6,4 +6,21 @@ const dbconfig = {
     user: "root",
     password: process.env.PASSWORD
 }
-console.log(process.env.PASSWORD);
+
+let dbConnect = async (obj) => {
+    try {
+        await mysql.createConnection(obj).connect(e => {
+            if (e) {
+                console.log(e);
+            } else {
+                console.log("Connection Successful");
+            }
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+dbConnect(dbconfig);
+
+
